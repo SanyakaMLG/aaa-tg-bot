a7e518bb06a22315993ba752dc807eb0c2bff359
SanyakaMLG <alekseev.sasha0204@yandex.ru>
2024-01-01 15:08:17 +0300

    rewrite won func

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,14 @@
 def won(state: list[list[str]]) -> bool:
     """Check if crosses or zeros have won the game"""
-    rows = [set(state[i][j] for i in range(3)) for j in range(3)]
-    cols = [set(state[i][j] for j in range(3)) for i in range(3)]
-    diags = [
-        set(state[i][i] for i in range(3)),
-        set(state[i][2 - i] for i in range(3))
-    ]
-    for row in rows:
-        if len(row) == 1 and '.' not in row:
+    for i in range(3):
+        if state[i][0] == state[i][1] == state[i][2] != '.':
             return True
-    for col in cols:
-        if len(col) == 1 and '.' not in col:
+        if state[0][i] == state[1][i] == state[2][i] != '.':
             return True
-    for diag in diags:
-        if len(diag) == 1 and '.' not in diag:
-            return True
+    if state[0][0] == state[1][1] == state[2][2] != '.':
+        return True
+    if state[0][2] == state[1][1] == state[2][0] != '.':
+        return True
     return False
 
 
